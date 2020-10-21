83da93cff3ca8f2995ee915412fe0f43f6c7ebf0
jangseoyoung <jangseoyoung0402@gmail.com>
2020-10-21 21:15:00 +0900

    Update : Hansel
    Add : AddViewController

---
+++ Hansel/Hansel/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Hansel
+//
+//  Created by 장서영 on 2020/10/21.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.topItem?.title = "All Routes"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
