32d94a5cdee83d1adc142e27f9fd0b71e81571b4
jangseoyoung <jangseoyoung0402@gmail.com>
2020-11-04 23:16:02 +0900

    Create : Hansel
    create Model

---
+++ Hansel/Hansel/Model/DetailRoutes.swift
@@ -0,0 +1,12 @@
+//
+//  DetailRoutes.swift
+//  Hansel
+//
+//  Created by 장서영 on 2020/11/04.
+//
+
+import Foundation
+
+struct DetailRoutes{
+    var 
+}
---
+++ Hansel/Hansel/Model/PreRoutes.swift
@@ -0,0 +1,17 @@
+//
+//  PreRoutes.swift
+//  Hansel
+//
+//  Created by 장서영 on 2020/11/04.
+//
+
+import Foundation
+
+struct PreRoutes{
+    var maindate : String = ""
+    var count : Int = 0
+    
+    init(date : String, count : String){
+        
+    }
+}
